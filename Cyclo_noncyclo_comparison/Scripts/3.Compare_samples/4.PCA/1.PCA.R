9435e70e6749e06afe53f51a651ce645e1f93186
Yong-Han Hank Cheng <yhhc@n3283.hyak.local>
2024-05-20 12:45:37 -0700

    Last commit before modifying script to take non-hardcoded inputs

--- Cyclo_noncyclo_comparison/Scripts/3.Compare_samples/4.PCA/1.PCA.R
+++ Cyclo_noncyclo_comparison/Scripts/3.Compare_samples/4.PCA/1.PCA.R
@@ -1,6 +1,6 @@
 # Usage: 
 # conda activate r_env_per_isoform
-# Rscript PCA.R > PCA_output.txt 2>&1
+# Rscript 1.PCA.R > 1.PCA_output.txt 2>&1
 
 #setwd("~/2022-2023/Research/Computational/Isoform counting/Per_isoform_analysis/4.28.24_Comparison/Isoseq_comparison_cyclo_vs_noncyclo_4.28.24/PCA")
 
@@ -11,13 +11,13 @@ library(ggplot2)
 count_threshold <- 6
 
 # Specify the path to the CSV file
-file_path <- "data_combined_full.csv"
+file_path <- "/mmfs1/gscratch/stergachislab/yhhc/projects/Iso-seq_public/Cyclo_noncyclo_comparison/Scripts/3.Compare_samples/3.Novel_iso_abundance_in_gene/data_combined_full_gene_with_Hyp5.csv"
 
 # Read the CSV file into a data table
 dt <- fread(file_path)
 
 # Remove BCH_1199-01 because it's an outlier
-dt <- dt[Sample != "BCH_1199-01"]
+# dt <- dt[Sample != "BCH_1199-01"]
 
 # Print the first few rows of the data table
 print(head(dt))
@@ -81,7 +81,7 @@ pca_plot <- ggplot(pca_data, aes(x = PC1, y = PC2, color = Type, label = Sample)
   geom_point(alpha = 0.8, size = 4) +
   geom_text(aes(label = Sample), vjust = -0.1, hjust = -0.1, size = 2, color = "black") +  # Adjust label position and size
   scale_color_manual(values = c("cyclo" = "blue", "noncyclo" = "red")) +
-  labs(title = "PCA Plot Isoform-Level removed BCH_1199",
+  labs(title = "PCA Plot Gene-Level",
        x = paste("PC1 (", variance_explained[1], "% variance)", sep = ""),
        y = paste("PC2 (", variance_explained[2], "% variance)", sep = ""),
        color = "Sample Type") +
@@ -91,4 +91,4 @@ pca_plot <- ggplot(pca_data, aes(x = PC1, y = PC2, color = Type, label = Sample)
 print(pca_data)
 
 # Save the plot
-ggsave("PCA_isoform_level_removed_BCH1199.pdf", plot = pca_plot, width = 8, height = 6)
+ggsave("PCA_Gene_level_with_Hyp5_data_combined.pdf", plot = pca_plot, width = 8, height = 6)
