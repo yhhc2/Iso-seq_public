f45de22c0d14015707ad0478a1cb3b6c24ddcba0
Hank Cheng <70345215+yhhc2@users.noreply.github.com>
2024-06-15 11:25:06 -0700

    Update Replicate_analysis.R

--- Cyclo_noncyclo_comparison/Scripts/3.Compare_samples/5.Replicate_analysis/Replicate_analysis.R
+++ Cyclo_noncyclo_comparison/Scripts/3.Compare_samples/5.Replicate_analysis/Replicate_analysis.R
@@ -15,7 +15,7 @@ library(ggplot2)
 args <- commandArgs(trailingOnly = TRUE)
 
 # Check for correct number of arguments
-if (length(args) != 2) {
+if (length(args) != 5) {
   stop("Usage: Rscript Replicate_analysis <file_path> <count_threshold> <plot_title> <samples> <TPM or NormalizedFractionDifference>", call. = FALSE)
 }
 
